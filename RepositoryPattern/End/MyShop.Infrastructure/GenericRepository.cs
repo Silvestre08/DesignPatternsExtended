a2f9b0b0726c24f47c6e9ffcab16c7377c233c30
Celso <celsosilvestre08@gmail.com>
2024-06-26 21:04:15 -0400

    introduce generic repository

---
+++ RepositoryPattern/End/MyShop.Infrastructure/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Infrastructure
+{
+    internal abstract class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected ShoppingContext Context;
+
+        public GenericRepository(ShoppingContext context)
+        {
+            Context = context;
+        }
+
+        public T Add(T entity) => Context.Add(entity).Entity;
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+           return Context.Set<T>().AsQueryable().Where(predicate);
+        }
+
+        public T Get(Guid id)
+        {
+           return Context.Find<T>(id);
+        }
+
+        public IEnumerable<T> GetAll() 
+            => Context.Set<T>().ToList();
+
+        public virtual void SaveChanges()
+        {
+            Context.SaveChanges();
+        }
+
+        public virtual T Update(T entity) => Context.Update(entity).Entity;
+    }
+}
---
+++ RepositoryPattern/End/MyShop.Infrastructure/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using MyShop.Domain.Models;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyShop.Infrastructure
+{
+    internal class ProductRepository : GenericRepository<Product>
+    {
+        public ProductRepository(ShoppingContext context) : base(context)
+        {
+             
+        }
+
+        public override Product Update(Product entity)
+        {
+            var product = Context.Products.Single(p => p.ProductId == entity.ProductId);
+            product.Name = entity.Name;
+            product.Price = entity.Price;
+            return base.Update(entity);
+        }
+    }
+}
